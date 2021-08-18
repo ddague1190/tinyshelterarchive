5abd1f4472b744c9b9eb53860f1b8c0f85f62d43
Darryl Dague <ddague77@gmail.com>
2021-08-18 19:01:48 -0400

    gunicorn install

--- final/heroku.py
+++ final/heroku.py
@@ -13,7 +13,8 @@ env = environ.Env(
 )
 
 # False if not in os.environ
-DEBUG = env('DEBUG')
+# DEBUG = env('DEBUG')
+DEBUG = True
 
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
