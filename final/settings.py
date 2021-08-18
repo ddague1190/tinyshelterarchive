a74b1245b0c7ba7c1a4122d8798e7f15f1468712
Darryl Dague <ddague77@gmail.com>
2021-08-18 07:21:17 -0400

    remove STATICFILES_DIRS definition

--- final/settings.py
+++ final/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'final/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'final/static'),
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #AUTH_USER_MODEL = "website.User"
