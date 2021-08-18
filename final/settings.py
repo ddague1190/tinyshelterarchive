5de6b6fe314b5b6c304ab3dde04bcd6e74bba6fa
Darryl Dague <ddague77@gmail.com>
2021-08-18 06:22:47 -0400

    Updated password validation

--- final/settings.py
+++ final/settings.py
@@ -118,10 +118,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = []
-
-"""
-    {
+AUTH_PASSWORD_VALIDATORS = [{
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
@@ -132,7 +129,9 @@ AUTH_PASSWORD_VALIDATORS = []
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },"""
+    },
+    ]
+
 
 
 
