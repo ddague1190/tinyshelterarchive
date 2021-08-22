c204196ba1168b85241be5f89052dc58c25b7a67
Darryl Dague <ddague77@gmail.com>
2021-08-22 18:49:53 -0400

    add allowed hosts

--- final/heroku.py
+++ final/heroku.py
@@ -18,7 +18,15 @@ DEBUG = env('DEBUG')
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+ALLOWED_HOSTS = [
+    'tinyshelterarchive.com',
+    'project-tsa.herokuapp.com'
+
+
+]
+
 
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
